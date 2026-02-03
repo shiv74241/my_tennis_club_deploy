865615cd226c4f6f491e3001cc2b143b49198181
shiv74241 <powerqueryprudent@gmail.com>
2026-02-03 20:26:04 +0530

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# Tell Django where your settings are
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_tennis_club.settings')
-
-# This is the "app" instance the server uses
-application = get_wsgi_application()
