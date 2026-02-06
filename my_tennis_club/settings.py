f917bfe0f9e077d91bc64d67f12af97336c0ab64
Shiv Kumar <shiv74241@gmail.com>
2026-02-06 20:08:11 +0530

    Add auto superuser creation for Render

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -98,14 +98,14 @@ import dj_database_url
 
 
 
-import os
-import dj_database_url
-
 DATABASES = {
     "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
+
 
 
 
