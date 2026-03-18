6c0aac262690d7f6fa2e1100e1f634634c10901d
Shiv Kumar <shiv74241@gmail.com>
2026-03-18 20:10:03 +0530

    add admin display list visitor2

--- members/admin.py
+++ members/admin.py
@@ -21,3 +21,37 @@ class MemberAdmin3(admin.ModelAdmin):
 admin.site.register(Member,MemberAdmin)
 admin.site.register(student,MemberAdmin2)
 admin.site.register(ContactMessage,MemberAdmin3)
+
+
+from django.contrib import admin
+from .models import Visitor2
+
+@admin.register(Visitor2)
+class Visitor2Admin(admin.ModelAdmin):
+    list_display = (
+        'visitor_id',
+        'ip_address',
+        'browser',
+        'os',
+        'device_type',
+        'country',
+        'city',
+        'created_at'
+    )
+
+    list_filter = (
+        'browser',
+        'os',
+        'device_type',
+        'country',
+    )
+
+    search_fields = (
+        'visitor_id',
+        'ip_address',
+        'user_agent',
+        'country',
+        'city'
+    )
+
+    list_per_page = 50
